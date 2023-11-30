c557cc41760cf9af512f3fca91be44e4c4fe4b53
Arthur Santos <santos.santos86@hotmail.com>
2023-11-30 16:09:35 -0300

    context api and firebase and auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css";
+import { AppProvider } from "../data/context/AppContext";
+import { AuthProvider } from "../data/context/AuthContext";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </AuthProvider>
+  );
 }
+
+export default MyApp;
